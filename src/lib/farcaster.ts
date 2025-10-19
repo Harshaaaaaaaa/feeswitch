4b4aeb681e25681535cc269ef51b0884828af55a
Harshaaaaaaaa <159971416+Harshaaaaaaaa@users.noreply.github.com>
2025-10-19 08:30:16 +0530

    Update farcaster.ts

--- src/lib/farcaster.ts
+++ src/lib/farcaster.ts
@@ -1,5 +1,4 @@
-import sdk from '@farcaster/frame-sdk';
-
+import { sdk } from '@farcaster/miniapp-sdk';
 export const initFarcasterSDK = async () => {
   try {
     const context = await sdk.context;
