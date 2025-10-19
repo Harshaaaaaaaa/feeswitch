8d5a522ccdd04697750f3b3e772176e7f8f74cbb
Harshaaaaaaaa <harshavardhann76@gmail.com>
2025-10-19 07:25:55 +0530

    Initial commit

---
+++ output/assets/secp256k1-JLYszVlB.js
@@ -0,0 +1,2 @@
+import { st as secp256k1 } from "./index-COr1gJpN.js";
+export { secp256k1 };
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import {StrictMode} from "react";
+import { ErrorBoundary } from './components/ErrorBoundary.tsx';
+
+createRoot(document.getElementById("root")!).render(
+        <StrictMode>
+            <ErrorBoundary>
+                <App />
+            </ErrorBoundary>
+        </StrictMode>
+);
